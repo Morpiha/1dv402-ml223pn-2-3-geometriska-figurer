73bf302bcebc0519ba40b577ea35de285c5fe967
morpiha <morpiha@live.se>
2014-10-16 14:14:59 +0200

    Commit 1

---
+++ ConsoleApplication1/Ellipse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Ellipse : Shape
+    {
+        public double Ellipse(double lenght, double width)
+        {
+            return 0;
+        }
+        public double Area
+        {
+            get
+            {
+                return Area;
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return Perimeter;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Shape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    abstract class Shape
+    {
+        private double _length;
+        private double _width;
+
+        protected Shape(double lenght, double width)
+        {
+            lenght = _length;
+            width = _width;
+            
+        }
+        public override string ToString()
+        {
+            return string.Format("Something");
+        }
+
+        public double Area
+        {
+            get {
+                return Area;
+            }
+        }
+        public double Lenght
+        {
+            get
+            {
+                return Lenght;
+            }
+            set
+            {
+
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return Perimeter;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return Width;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
